00802855fabfc48eace5ed53328165ce25b3edb3
Christopher Bain <cbain@hornsolutions.net>
2019-03-25 13:58:21 -0500

    Products

--- CPQUtilities/Product.cs
+++ CPQUtilities/Product.cs
@@ -9,15 +9,16 @@ namespace CPQUtilities
 {
     public class Product
     {
-        public string DisplayType { get; set; }
-        public bool SkipCategoriesOnProductUpdate { get; set; }
-        public bool SkipPermissionsOnProductUpdate { get; set; }
-        public string Identifier { get; set; }
-        public string ShippingCosts { get; set; }
+        //not required:
+        public string DisplayType { get; set; } //supports values: Simple, Configurable, System, Collection, Parent_child; default type is "Simple product"
+        public bool SkipCategoriesOnProductUpdate { get; set; } //default value (if attribute is missing): false
+        public bool SkipPermissionsOnProductUpdate { get; set; } //default value (if attribute is missing): false
+        public string Identificator { get; set; } //default value (if node is missing): PartNumber
+        public string ShippingCosts { get; set; } //Default Value (if node is missing): Empty
         public string Permissions { get; set; } //if permissions node is present in XML and is empty, product will not be visible to any permission group
         public string CPQProductId { get; set; } //when product is being created, this node is not required; if any value is sent through this node, it will be ignored
-        public string PartNumber { get; set; }
-        public string UPC { get; set; }
+        public string PartNumber { get; set; } //Default Value(if node is missing): Empty
+        public string UPC { get; set; } //Default value (if node is missing): Empty
         public string MPN { get; set; }
         public string ProductFamilyCode { get; set; }
         public string RecurringPriceFormula { get; set; }
@@ -41,28 +42,61 @@ namespace CPQUtilities
         public string PriceFormual { get; set; }
         public string CostFormula { get; set; }
         public string Description { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
-        //public string BaseRecurringPrice { get; set; }
+        public string LongDescription { get; set; }
+        public string CartDescription { get; set; }
+        public string Attributes { get; set; }
+        public string AttributeType { get; set; }
+        public string AttributeDisplayType { get; set; }
+        public string AttributeMeasurementType { get; set; }
+        public string AttributeRank { get; set; }
+        public string AttributeLineItem { get; set; }
+        public string AttributeLineItemDescription { get; set; }
+        public string AttributeRankWithinCart { get; set; }
+        public string AttributeSpansAcrossEntireRow { get; set; }
+        public string AttributeRequired { get; set; }
+        public string AttributeLabel { get; set; }
+        public string AttributeHint { get; set; }
+        public string AttributeShowOneTimePrice { get; set; }
+        public string AttributeShowRecurringPrice { get; set; }
+        public string AttributeButtonText { get; set; }
+        public string AttributeAttachScriptButton { get; set; }
+        public string AttributeButtonScriptsAttached { get; set; }
+        public string AttributeButtonScript { get; set; }
+        public string ButtonScriptAttachedRank { get; set; }
+        public string AttributeValuesPreselected { get; set; }
+        public string Tabs { get; set; }
+        public string TabsSystemId { get; set; }
+        public string TabsName { get; set; }
+        public string TabsProductTabRank { get; set; }
+        public string TabsLayoutTemplate { get; set; }
+        public string TabsVisibilityPermission { get; set; }
+        public string TabsVisibilityCondition { get; set; }
+        public string TabsShowTabHeader { get; set; }
+        public string TabsAttributes { get; set; }
+        public string TabsAttributesName { get; set; }
+        public string TabsAttributesRank { get; set; }
+        public string GlobalScripts { get; set; }
 
 
-
+        //required fields:
         public string ProductType { get; set; }
         public Translations ProductName { get; set; }
-        
         public CategoryList Categories { set; get; }
+        public string CategoryListString { get; set; }
+
 
         public Product()
         {
             //set defaults here
             //http://help.webcomcpq.com/doku.php?id=appendixd:simple_product_administration:input_xml_example
             Permissions = "Visible to all";
+            SkipCategoriesOnProductUpdate = false;
+            SkipPermissionsOnProductUpdate = false;
+            Identificator = PartNumber;
+            ShippingCosts = "";
+            Permissions = "Visible to all";
+            PartNumber = "";
+            UPC = "";
 
 
         }
@@ -72,28 +106,22 @@ namespace CPQUtilities
             //http://help.webcomcpq.com/doku.php?id=appendixd:simple_product_administration:input_xml_example
 
             XmlDocument retVal = new XmlDocument();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<Products SkipCategoriesOnProductUpdate='" + SkipCategoriesOnProductUpdate.ToString().ToLower() +"' SkipPermissionsOnProductUpdate='" + SkipPermissionsOnProductUpdate.ToString().ToLower() +"' >");
+            sb.AppendLine("<Product>");
+            sb.AppendLine("<Identificator>" + Identificator + "</Identificator>");
+            sb.AppendLine("<DisplayType>" + DisplayType + "</DisplayType>");
+            sb.AppendLine("<PartNumber>" + PartNumber + "</PartNumber>");
+            sb.AppendLine("<ProductType>" + ProductType + "</ProductType>");
+            sb.AppendLine("<ProductName><USEnglish><![CDATA[{" + ProductName + "}]]></USEnglish></ProductName>");
+            sb.AppendLine("<StartDate>" + StartDate.ToString() + "</StartDate>");
+            sb.AppendLine("<EndDate>" + EndDate.ToString() + "</EndDate>");
+            sb.AppendLine("<Categories><USEnglish><![CDATA[{" + CategoryListString + "}]]></USEnglish></Categories>");
+            sb.AppendLine("</Product>");
+            sb.AppendLine("</Products>");
 
-            //generate xml structure based on fields that are filled in
-                          // XmlDocument xDoc = new XmlDocument();
-            //                retVal.LoadXml(string.Format(@"
-            //<Products>
-            //    <Product>
-            //        <Identificator>{0}</Identificator>
-            //        <DisplayType>{1}</DisplayType>
-            //        <PartNumber>{2}</PartNumber>
-            //        <ProductType>{3}</ProductType>
-            //        <ProductName>
-            //            <USEnglish><![CDATA[{4}]]></USEnglish>
-            //        </ProductName>
-            //        <StartDate>{5}</StartDate>
-            //        <EndDate>{6}</EndDate>
-            //        <Categories>
-            //            <USEnglish><![CDATA[{7}]]></USEnglish>
-            //        </Categories>
-            //    </Product>
-            //</Products>
-            //", ProductId, Identifier, DisplayType, PartNumber, ProductType, ProductName, StartDate, EndDate, Categories));
 
+            retVal.LoadXml(sb.ToString());
             return retVal;
         }
 
