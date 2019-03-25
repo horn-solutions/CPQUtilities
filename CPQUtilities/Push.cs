d908d7c5376a2d3aaccae7976209dd96a3ec7b88
Christopher Judd <cjudd@hornsolutions.net>
2019-03-25 07:09:53 -0500

    Initial projects created

---
+++ CPQUtilities/Push.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPQUtilities
+{
+    public static class Push
+    {
+        public static bool Product(string name)
+        {
+            return false;
+        }
+    }
+}
---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using CPQUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding Product1");
+            bool result = Push.Product("product1");
+
+
+
+            Console.WriteLine(string.Format("Product1 push response: {0}", result));
+            Console.ReadLine();
+        }
+    }
+}
