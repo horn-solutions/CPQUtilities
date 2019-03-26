b2ef7f9e3620fec2619d0368caf626ec2425e3af
Christopher Judd <cjudd@hornsolutions.net>
2019-03-26 06:53:09 -0500

    Added Utility

---
+++ CPQUtilities/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CPQUtilities
+{
+    public static class Utility
+    {
+        internal static XmlNode AddIfNotEmptyOrNull(XmlNode parentNode, string nodeName, object value)
+        {
+            if (value == null)
+                return null;
+
+            XmlNode newNode = parentNode.OwnerDocument.CreateElement(nodeName);
+
+            switch (value.GetType().Name)
+            {
+                case "String":
+                    newNode.InnerText = (string)value;
+                    parentNode.AppendChild(newNode);
+                    break;
+
+                case "DateTime":
+                    newNode.InnerText = ((DateTime)value).ToString();
+                    parentNode.AppendChild(newNode);
+                    break;
+
+                case "Int32":
+                    newNode.InnerText = ((int)value).ToString();
+                    parentNode.AppendChild(newNode);
+                    break;
+
+                default:
+                    System.Diagnostics.Debug.WriteLine(value.GetType().Name);
+                    break;
+            }
+
+            return newNode;
+        }
+    }
+}
--- ConsoleTester/JuddTest.cs
+++ ConsoleTester/JuddTest.cs
@@ -14,7 +14,15 @@ namespace ConsoleTester
             Credentials c = new Credentials();
             Console.Write(string.Format("Logging in as {0}", c.Login));
             Console.WriteLine(string.Format(" ... Login {0}", c.DoYouSeeMe() ? "Successful" : "Unsuccessful"));
+            User u = new User()
+            {
+                Username="login",
+                Password="123123",
+                FirstName="Christopher",
+                LastName="Judd"
+            };
 
+            Console.WriteLine(u.CreateXml().InnerXml);
             
             Console.ReadLine();
 
