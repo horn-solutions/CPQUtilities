400e60d86fb408608baacb6830ad719368e6b1dc
Christopher Bain <cbain@hornsolutions.net>
2019-04-05 13:20:03 -0500

    Added SearchQuotes method to Get

--- CPQUtilities/Get.cs
+++ CPQUtilities/Get.cs
@@ -10,7 +10,7 @@ namespace CPQUtilities
     public static class Get
     {
 
-        
+        //login issues persist for CPQQPI login attempts.
 
         public static String Catalogue(Credentials credentials)
         {
@@ -39,6 +39,36 @@ namespace CPQUtilities
 
 
             return retVal;
+        }
+
+        public static String SearchQuotes(Credentials credentials, String SearchCriteriaXML)
+        {
+            string retVal = "";
+
+        //http://help.webcomcpq.com/doku.php?id=appendixd:search_quotes
+        // this method enables the external system to retrieve quotes from SAP CPQ based on provided search criteria. 
+        //This method is only available if SAP CPQ user account that is used to make a call is set as an administrative account. 
+        //The maximum number of quotes that can be returned is 2000. 
+
+//        Examples:
+
+//            DATE_MODIFIED > '8/3/2011 13:50:20'
+//        Returns all quotes that have been modified after this date / time
+//    DATE_CREATED > '9/24/2011 00:00:00'
+//        Returns all quotes that have been created after this date / time
+//    DATE_MODIFIED > '8/3/2011 13:50:20' AND ORDER_STATUS = '2'
+//        Returns all quotes that have been modified after this date / time that are in status ‘Order Placed’
+
+//If Criteria for search is empty, SAP CPQ will return all quotes(top 2000 quotes).
+
+            CpqApi.CpqApi cpq_service = new CpqApi.CpqApi();
+            cpq_service.Timeout = 200 * 1000;
+
+            retVal = cpq_service.SearchQuotes(credentials.Login, credentials.Password, SearchCriteriaXML);
+
+            return retVal;
+
+
         }
     }
 }
